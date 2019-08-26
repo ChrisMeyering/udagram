08de6cad2cf22f34358751fe9c339af59c89267b
Chris Meyering <christophe.daven@gmail.com>
2019-08-26 09:21:03 -0700

    style: reformatted server.ts

--- image-filter-starter-code/src/server.ts
+++ image-filter-starter-code/src/server.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 import { url } from 'inspector';
 
 (async () => {
@@ -10,7 +10,7 @@ import { url } from 'inspector';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -27,41 +27,41 @@ import { url } from 'inspector';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-  
+
 
 
   app.get('/filteredimage/',
-  async (req: Request, res: Response) => {
-  let {image_url} = req.query;
-  if (!image_url) {
-    return res.status(400).send('Image url is required');
-  }
-  filterImageFromURL(image_url).catch(e => {
-    return res.status(400).send('Unable to filter image.')
-  }).then(filePath => {
-    if (filePath.toString() == "error") {
-      return res.status(400).send("Image url is invalid")
-    }
-    return res.on('finish', () => {
-      var fileArray:string[] = [filePath.toString()]
-     deleteLocalFiles(fileArray);
-    }).status(200).sendFile(filePath.toString());
-  });
-});
-/**************************************************************************** */
+    async (req: Request, res: Response) => {
+      let { image_url } = req.query;
+      if (!image_url) {
+        return res.status(400).send('Image url is required');
+      }
+      filterImageFromURL(image_url).catch(e => {
+        return res.status(400).send('Unable to filter image.')
+      }).then(filePath => {
+        if (filePath.toString() == "error") {
+          return res.status(400).send("Image url is invalid")
+        }
+        return res.on('finish', () => {
+          var fileArray: string[] = [filePath.toString()]
+          deleteLocalFiles(fileArray);
+        }).status(200).sendFile(filePath.toString());
+      });
+    });
+  /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
